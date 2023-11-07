e304e49a3e1b5e1e262230365f615b313868403d
Joshgg1 <93443648+Joshgg1@users.noreply.github.com>
2023-11-07 09:47:33 -0600

    Update shoppingClass.java

--- shoppingClass.java
+++ shoppingClass.java
@@ -1,1 +1,45 @@
 
+
+public class shoppingClass {
+
+
+   private int numItems;
+
+
+   public int getNumItems() {
+       return numItems;
+   }
+  
+
+
+   public void setNumItems(int numItems) {
+       this.numItems = numItems;
+   }
+
+
+   private String objects;
+  
+   public String getObjects() {
+       return objects;
+   }
+
+
+   public void setObjects(String objects) {
+       this.objects = objects;
+   }
+
+
+   private String price;
+
+
+   public String getPrice() {
+       return price;
+   }
+
+
+   public void setPrice(String price) {
+       this.price = price;
+   }
+
+
+}
